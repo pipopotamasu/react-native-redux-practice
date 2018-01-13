166fdffecb1f77a9c8691a816ec96a0bc22e347b
pipopotamasu <ec0156hx@gmail.com>
2018-01-13 13:28:12 +0900

    add counter without redux

---
+++ src/components/Counter.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import {
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+  Text,
+  View
+} from 'react-native';
+
+export default class Counter extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0,
+    };
+  }
+
+  countUp = () =>  {
+    this.setState({
+      count: ++this.state.count
+    });
+
+    console.log(this.state.count);
+  }
+
+  render() {
+    const {
+      count,
+    } = this.state;
+
+    return (
+      <View>
+        <Text>{ count }</Text>
+        <TouchableOpacity
+          onPress={this.countUp}
+        >
+          <Text>Counter</Text>
+        </TouchableOpacity>
+      </View>
+    )
+  }
+}
