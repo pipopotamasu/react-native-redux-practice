b41f9ac7f7dec8495240e112458691c3ff9f07e5
pipopotamasu <ec0156hx@gmail.com>
2018-01-13 13:42:09 +0900

    regist provider

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 import { AppRegistry } from 'react-native';
 import App from './src/App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux/native';
+import Counter from './reducers/counter.js';
 
-AppRegistry.registerComponent('react_native_redux_practice', () => App);
+let store = createStore(Counter)
+
+let ReduxApp = React.createClass({
+  render: function() {
+    return(
+      <Provider store={store}>
+       {() => <App />}
+      </Provider>
+    );
+  }
+});
+
+AppRegistry.registerComponent('react_native_redux_practice', () => ReduxApp);
