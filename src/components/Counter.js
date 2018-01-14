bb4a66aa4adbefd9ece12fa7587e63cb28541ad6
pipopotamasu <ec0156hx@gmail.com>
2018-01-14 11:15:22 +0900

    button style

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -11,23 +11,53 @@ export default Counter = (props) => {
   const {
     onCountUp,
     onCountDown,
-    onInputCount
+    onInputCount,
     count
   } = props;
 
   return (
-    <View>
-      <Text>count: { count }</Text>
-      <TouchableOpacity
-        onPress={onCountUp}
-      >
-        <Text>Count Up</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        onPress={onCountDown}
-      >
-        <Text>Count Down</Text>
-      </TouchableOpacity>
+    <View style={styles.container}>
+      <View>
+        <Text>count: { count }</Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          onPress={onCountUp}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Count Up</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={onCountDown}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Count Down</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+  },
+  // textInput: {
+  //   flex: 3,
+  //   backgroundColor: '#FFF',
+  //   marginRight: 5,
+  // },
+  button: {
+    backgroundColor: '#4169E1',
+    marginLeft: 5,
+    justifyContent: 'center',
+    padding: 5
+  },
+  buttonText: {
+    color: '#FFF',
+    fontWeight: '500',
+  }
+});
